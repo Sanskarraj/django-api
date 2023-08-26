e4f3610d99f04294ee45d3fcb6e322daf189fa88
“[firstname <sanskarsrs9@gmail.com>
2023-08-26 18:23:45 +0530

    deploy

--- notes/settings.py
+++ notes/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-wx&^j9dkg7av-9o7)utfsx_!9hg9oq*k47ypwo4l$vq@k4na%3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -128,3 +128,9 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS=True
+
+
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,"static"),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
